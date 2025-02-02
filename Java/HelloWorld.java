a5080ccb6a89047c41eeeb23a5c62509a5ec18c5
Ram <ramprathap221@gmail.com>
2025-02-02 23:55:17 +0530

    first java program

---
+++ Java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static int addTwoNumbers(int a,int b){
+        return a+b;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(5, 6));
+    }
+}
